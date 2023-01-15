e63a142e07624a1012d7e3694cfeef34d5341ac1
Daniel Konecny <daniel.konecny@broadcom.com>
2023-01-15 21:46:17 +0100

    move input into _process() function

--- src/player.rs
+++ src/player.rs
@@ -26,6 +26,7 @@ pub struct Player {
     jump_impulse: f32,
 
     velocity: Vector3,
+    mouse_look: Vector2,
     player_pivot: OptRef<Spatial>,
     cam_x_rot: f32,
     cam_pivot: OptRef<Spatial>,
@@ -40,6 +41,7 @@ impl Player {
             fall_acceleration: 75.0,
             jump_impulse: 20.0,
             velocity: Vector3::ZERO,
+            mouse_look: Vector2::ZERO,
             player_pivot: OptRef::None,
             cam_x_rot: 0.0,
             cam_pivot: OptRef::None,
@@ -63,17 +65,22 @@ impl Player {
         // camera handling by mouse movement
         match event.try_cast::<InputEventMouseMotion>() {
             Ok(e) => unsafe {
-                self.rotate_cam(e.assume_safe().relative() * CAMERA_MOUSE_SPEED);
+                self.mouse_look = e.assume_safe().relative();
             },
             Err(_) => {}
         };
     }
 
     #[method]
-    fn _physics_process(&mut self, #[base] base: &KinematicBody, delta: f64) {
+    fn _process(&mut self, #[base] base: &KinematicBody, _delta: f64) {
         self.process_input(base);
+        // zero out mouse, so it doesn't keep rotating
+        self.mouse_look = Vector2::ZERO;
+    }
 
-        // or last parameter `false` to interact with RigidBody?
+    #[method]
+    fn _physics_process(&mut self, #[base] base: &KinematicBody, delta: f64) {
+        // move only when there is velocity or is not on ground
         if !base.is_on_floor() || self.velocity.length_squared() > 0.0 {
             // apply gravity and move
             self.velocity.y -= self.fall_acceleration * (delta as f32);
@@ -92,8 +99,13 @@ impl Player {
         let direction = base.transform().basis * Vector3::new(inp.x, 0.0, inp.y);
 
         // handle camera
-        let look = input.get_vector("look_left", "look_right", "look_up", "look_down", -1.0);
-        self.rotate_cam(look * CAMERA_CONTROLLER_SPEED);
+        let controller_look =
+            input.get_vector("look_left", "look_right", "look_up", "look_down", -1.0);
+        if controller_look.length_squared() > 0.0 {
+            self.rotate_cam(controller_look * CAMERA_CONTROLLER_SPEED);
+        } else if self.mouse_look.length_squared() > 0.0 {
+            self.rotate_cam(self.mouse_look * CAMERA_MOUSE_SPEED);
+        }
 
         // handle jump
         if base.is_on_floor() && input.is_action_just_pressed("jump", false) {
@@ -106,8 +118,8 @@ impl Player {
 
     fn update_velocity(&mut self, direction: Vector3) {
         let mut direction = direction;
-        // adjust movement according to camera basis
         let dir_len = direction.length();
+        // process movement depending on the input
         if dir_len > 0.0 {
             // adjust direction according to camera if it exists
             if let OptRef::Some(_) = self.cam_pivot {
@@ -128,6 +140,7 @@ impl Player {
             );
             self.player_pivot.tref().set_rotation(player_rot);
         } else {
+            // come to stop
             self.velocity.x = move_toward(self.velocity.x..=0.0, self.speed);
             self.velocity.z = move_toward(self.velocity.z..=0.0, self.speed);
         }
